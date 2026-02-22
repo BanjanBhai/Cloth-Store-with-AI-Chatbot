5ee9f665973eb607e963c63e665f2f2c1d092830
M_Faizan <125587636+BanjanBhai@users.noreply.github.com>
2026-02-22 17:34:32 +0500

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",         # Your MySQL username
-        password="iphone6S369248!mysql", # Your MySQL password
+        password="your-password-here", # Your MySQL password
         database="cloth_store" # Your MySQL database name
     )
     return connection 
