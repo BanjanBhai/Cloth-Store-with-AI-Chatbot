89c3e1c3eeed7ebcc06aeea445e27bccd505caa5
BanjanBhai <jadeboy160904@gmail.com>
2026-08-14 03:13:33 +0500

    .

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -6,7 +6,7 @@ import google.generativeai as genai
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
-# Configure Gemini
+# Configure Gemini #
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-2.5-flash')
 
